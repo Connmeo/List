03c7255247c771eb6cd195da3824aae60f9a2e13
Connmeo <37551420+Connmeo@users.noreply.github.com>
2019-05-12 19:07:24 +0300

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,30 +1,27 @@
-import React, { useState ,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
-import {Articles } from './Articles';
+import { Articles } 
 
-const Home = ({ id, setNewtArticle }) = > { //Почему тут выдает ошибку?
-    const [articles, setArticle] = useState([]);
-    
-    useEffect(()=> {
-        const new_article =Arricles();
+const Home = props => {
+  const [articles, setArticle] = useState([]);
 
-        setArticle(new_article);
+  useEffect(() => {
+    const new_article = Articles();
 
-    }, []);
+    setArticle(new_article);
+  }, []);
 
-    const setNewtArticle = code => { //что тут значит code?
-    setArticle(code);
-    };
+  const setNewtArticle = code => {
+    setArticle('name');
+  };
 
-    return (
-        <div className={id}>
-{articles.map(({name}) => 
-        <p>{article>}</p>
-        )
-        }
-        
-        </div>
-    )
-}
+  return (
+    <div className={id}>
+      {articles.map(({ name }) => (
+        <p>{article}</p>
+      ))}
+    </div>
+  );
+};
 
 export default Home;
